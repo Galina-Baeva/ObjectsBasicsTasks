61c2d92269a411f4f47d85931bac91093ff1fa1c
Galina-Baeva <galina.baeva@sperasoft.com>
2021-07-06 14:32:37 +0300

    corrected according to the comments

--- src/thirdTask/Dog.java
+++ src/thirdTask/Dog.java
@@ -1,6 +1,5 @@
 package thirdTask;
 
-import java.security.SecureRandom;
 import java.util.Random;
 
 public class Dog {
@@ -8,50 +7,16 @@ public class Dog {
     DogsSize size;
     int age;
 
-    Random random = new Random();
-
-    public Dog() {
-        setRandomName();
-        size = DogsSize.getRandomDodSize();
-        age = random.nextInt(20);
-    }
-
     public Dog(String name, DogsSize size, int age) {
         this.name = name;
         this.size = size;
         this.age = age;
     }
 
-    public Dog(String name) {
-        this.name = name;
-        size = DogsSize.getRandomDodSize();
-        age = random.nextInt(20);
-    }
-
-    public Dog(DogsSize size) {
-        setRandomName();
-        this.size = size;
-        age = random.nextInt(20);
-    }
-
-    public Dog(int age) {
-        setRandomName();
-        size = DogsSize.getRandomDodSize();
-        this.age = age;
-    }
 
     void printDog() {
         System.out.println(name + " " + size + " " + age);
     }
 
-    void setRandomName () {
-        final String alfa = "abcdefghijklmnopqrstuvwxyz";
-        SecureRandom random = new SecureRandom();
-        int nameLength = random.nextInt(10);
-        StringBuilder stringBuilder = new StringBuilder(nameLength);
-        for (int i=0; i<nameLength; i++)
-            stringBuilder.append(alfa.charAt(random.nextInt(alfa.length())));
-            name = stringBuilder.toString();
-    }
 
 }
--- src/thirdTask/DogsDemo.java
+++ src/thirdTask/DogsDemo.java
@@ -1,6 +1,9 @@
 package thirdTask;
+
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
 import java.util.Scanner;
 
 public class DogsDemo {
@@ -8,6 +11,10 @@ public class DogsDemo {
         int numberOfDogs = 0;
         String listOfDogs = " ";
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String name;
+        DogsSize size;
+        int age;
 
         System.out.println("Please enter the number of dogs and dogs' names (divided by space): ");
         listOfDogs = scanner.nextLine();
@@ -17,39 +24,45 @@ public class DogsDemo {
 
         for (int i = 0; i < numberOfDogs; i++) {
             if (i < listOfDogsParts.length - 1) {
-                setOfDogs[i] = new Dog(listOfDogsParts[i + 1]);
+                name = listOfDogsParts[i + 1];
             } else {
-                setOfDogs[i] = new Dog();
+                name = setRandomName();
             }
+            size = DogsSize.getRandomDodSize();
+            age = random.nextInt(20);
+            setOfDogs[i] = new Dog(name, size, age);
         }
         printAllDogs(setOfDogs);
 
-        System.out.println("Dogs from the youngest to the oldest");
-        Arrays.sort(setOfDogs, new Comparator<Dog>() {
-            @Override
-            public int compare(Dog o1, Dog o2) {
-                return o1.age - o2.age;
+        System.out.println("Please enter the number of dogs and dogs' ages (divided by space): ");
+        listOfDogs = scanner.nextLine();
+        String[] listOfDogsAgesParts = listOfDogs.split("\\s");
+        numberOfDogs = Integer.parseInt(listOfDogsAgesParts[0]);
+        Dog oneMoreSetOfDogs[] = new Dog[numberOfDogs];
+
+        for (int i = 0; i < numberOfDogs; i++) {
+            if (i < listOfDogsAgesParts.length - 1) {
+                age = Integer.parseInt(listOfDogsAgesParts[i + 1]);
+            } else {
+                age = random.nextInt(20);
             }
-        });
-        printAllDogs(setOfDogs);
+            name = setRandomName();
+            size = DogsSize.getRandomDodSize();
+            oneMoreSetOfDogs[i] = new Dog(name, size, age);
+        }
+        printAllDogs(oneMoreSetOfDogs);
+
+        System.out.println("Dogs from the youngest to the oldest");
+        sortByAge(oneMoreSetOfDogs);
+        printAllDogs(oneMoreSetOfDogs);
 
         System.out.println("Dogs sorted by name");
-        Arrays.sort(setOfDogs, new Comparator<Dog>() {
-            @Override
-            public int compare(Dog o1, Dog o2) {
-                return o1.name.compareToIgnoreCase(o2.name);
-            }
-        });
-        printAllDogs(setOfDogs);
+        sortByName(oneMoreSetOfDogs);
+        printAllDogs(oneMoreSetOfDogs);
 
         System.out.println("Dogs from the smallest to the biggest");
-        Arrays.sort(setOfDogs, new Comparator<Dog>() {
-            @Override
-            public int compare(Dog o1, Dog o2) {
-                return o1.size.compareTo(o2.size);
-            }
-        });
-        printAllDogs(setOfDogs);
+        sortBySize(oneMoreSetOfDogs);
+        printAllDogs(oneMoreSetOfDogs);
     }
 
     static void printAllDogs(Dog array[]) {
@@ -58,5 +71,41 @@ public class DogsDemo {
         }
     }
 
+    static String setRandomName() {
+        final String alfa = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+        SecureRandom random = new SecureRandom();
+        int nameLength = random.nextInt(10) + 2;
+        StringBuilder stringBuilder = new StringBuilder(nameLength);
+        for (int i = 0; i < nameLength; i++)
+            stringBuilder.append(alfa.charAt(random.nextInt(alfa.length())));
+        return stringBuilder.toString();
+    }
+
+    static void sortByAge(Dog[] dog) {
+        Arrays.sort(dog, new Comparator<Dog>() {
+            @Override
+            public int compare(Dog o1, Dog o2) {
+                return o1.age - o2.age;
+            }
+        });
+    }
+
+    static void sortByName(Dog[] dog) {
+        Arrays.sort(dog, new Comparator<Dog>() {
+            @Override
+            public int compare(Dog o1, Dog o2) {
+                return o1.name.compareToIgnoreCase(o2.name);
+            }
+        });
+    }
+
+    static void sortBySize(Dog[] dog) {
+        Arrays.sort(dog, new Comparator<Dog>() {
+            @Override
+            public int compare(Dog o1, Dog o2) {
+                return o1.size.compareTo(o2.size);
+            }
+        });
+    }
 
 }
